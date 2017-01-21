539a713c2bfe1abdcd86fe1bd8ff198c42b0a191
Caitlin <cbaker24601@gmail.com>
2017-01-21 17:55:07 -0500

    added pages and php
    added blank pages for entire site and now using php for includes.

---
+++ inc/header.php
@@ -0,0 +1,109 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Harvest Moon: Animal Parade</title>
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- /Bootstrap -->
+    </head>
+    <body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+          <a class="navbar-brand" href="index.php">
+            <img alt="Brand" src="inc/img/placeholder.png">
+          </a>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">The Basics <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+                <li><a href="controls.php">Game Controls</a></li>
+                <li><a href="main_menus.php">4 Main Menus</a></li>
+                <li><a href="walkthrough.php">Walkthrough</a></li>
+                <li><a href="new_game_plus.php">New Game +</a></li>
+                <li><a href="wiiconnect24.php">WiiConnect 24</a></li>
+                <li><a href="tools.php">Using Tools</a></li>
+                <li><a href="farm_degree.php">Farm Degree</a></li>
+                <li><a href="powerberry.php">Power Berries</a></li>
+          </ul>
+        </li>
+          <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">The People <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+                <li><a href="bachelorettes.php">Bachelorettes</a></li>
+                <li><a href="bachelors.php">Bachelors</a></li>
+                <li><a href="villagers.php">Villagers</a></li>
+                <li><a href="your_children.php">Your Children</a></li>
+                <li><a href="rival_children.php">Rival Kids</a></li>
+          </ul>
+        </li>
+          <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">The Farm <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+                <li><a href="crops.php">Crops</a></li>
+                <li><a href="trees.php">Trees</a></li>
+                <li><a href="livestock.php">Livestock</a></li>
+                <li><a href="poultry.php">Poultry</a></li>
+                <li><a href="pets.php">Pets</a></li>
+                <li><a href="house_upgrades.php">House Upgrades</a></li>
+                <li><a href="trophies.php">Trophies</a></li>
+                <li><a href="furniture.php">Furniture</a></li>
+                <li><a href="tv_shows.php">TV Shows</a></li>
+                <li><a href="cooking_basics.php">Cooking Basics</a></li>
+                <li><a href="cooking_recipes.php">Cooking Recipes</a></li>
+                <li><a href="clothing.php">Clothing</a></li>
+                <li><a href="rucksack.php">Bigger Rucksack</a></li>
+          </ul>
+        </li>
+          <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">The World <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+                <li><a href="shops_town.php">Town Shops</a></li>
+                <li><a href="shops_meadow.php">Meadow Shops</a></li>
+                <li><a href="shops_mountain.php">Mountain Shops</a></li>
+                <li><a href="shops_island.php">Island Shop</a></li>
+                <li><a href="world_map.php">World Map</a></li>
+                <li><a href="fishing.php">Fishing</a></li>
+                <li><a href="kingfish.php">The King Fish</a></li>
+                <li><a href="mining.php">Mining</a></li>
+                <li><a href="wild_items.php">Wild Items</a></li>
+          </ul>
+        </li>
+          <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">The Events <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+                <li><a href="your_marriage.php">Your Marriage</a></li>
+                <li><a href="rival_marriage.php">Rival Marriage</a></li>
+                <li><a href="events_request.php">Request Events</a></li>
+                <li><a href="events_random.php">Random Events</a></li>
+                <li><a href="events_children.php">Child Events</a></li>
+                <li><a href="festivals.php">Festivals</a></li>
+                <li><a href="circus.php">Theodore's Circus</a></li>
+          </ul>
+        </li>
+          <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">The Help <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+                <li><a href="credits.php">Game Credits</a></li>
+                <li><a href="faq.php">F.A.Q.</a></li>
+                <li><a href="calendar.php">Calendar</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
